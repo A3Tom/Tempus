45b207792d29c82f36bdcaf87a5c953e75656454
A3Tom <Tom_Esq@Yahoo.com>
2023-02-26 23:30:21 +0000

    Added basic application layer

---
+++ server/Tempus.Application/Commands/Event/UpsertEvent.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Tempus.Data.Abstract;
+using Tempus.Domain.Dtos;
+
+namespace Tempus.Application.Commands.Event;
+
+public class UpsertEvent : IRequestHandler<UpsertEvent.Request, EventDto>
+{
+    private readonly IEventRepository _eventRepository;
+
+    public UpsertEvent(IEventRepository eventRepository)
+    {
+        _eventRepository = eventRepository;
+    }
+
+    public Task<EventDto> Handle(Request request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public class Request : IRequest<EventDto>
+    {
+
+    }
+}
+
+
---
+++ server/Tempus.Application/Queries/Event/GetEventsByDateRange.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Tempus.Data.Abstract;
+
+namespace Tempus.Application.Queries.Event;
+public class GetEventsByDateRange : IRequestHandler<GetEventsByDateRange.Request, IEnumerable<EventDto>>
+{
+    private readonly IEventRepository _eventRepository;
+
+    public GetEventsByDateRange(IEventRepository eventRepository)
+    {
+        _eventRepository = eventRepository;
+    }
+
+    public async Task<IEnumerable<EventDto>> Handle(Request request, CancellationToken cancellationToken)
+    {
+        return await _eventRepository.GetEventsByDateRange(
+            request.StartDate,
+            request.EndDate,
+            request.UserId
+            );
+    }
+
+    public class Request : IRequest<IEnumerable<EventDto>>
+    {
+        public Request(DateTime startDate, DateTime? endDate, Guid? userId = null)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            UserId = userId;
+        }
+
+        public DateTime StartDate { get; init; }
+        public DateTime? EndDate { get; init; }
+        public Guid? UserId { get; init; }
+    }
+}
